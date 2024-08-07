d16676969542837cb450a440e12603df1a950c4d
hunit <hunit612@naver.com>
2024-08-07 23:25:43 +0900

    programmers

--- src/programmers/level0/P181860.java
+++ src/programmers/level0/P181860.java
@@ -1,5 +1,7 @@
 package programmers.level0;
 
+import java.util.ArrayList;
+
 /*
 아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 길이가 같은 정수 배열 arr과 boolean 배열 flag가 매개변수로 주어질 때, flag를 차례대로 순회하며 flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가하고, flag[i]가 false라면 X에서 마지막 arr[i]개의 원소를 제거한 뒤 X를 return 하는 solution 함수를 작성해 주세요.
 
@@ -33,4 +35,32 @@ arr	flag	result
 따라서 [3, 3, 3, 3, 4, 4, 4, 4]를 return 합니다.
  */
 public class P181860 {
+    public static void main(String[] args) {
+        int[] arr = {3,2,4,1,3};
+        boolean[] flag = {true, false, true, false, false};
+        solution(arr, flag);
+    }
+
+    public static int[] solution(int[] arr, boolean[] flag) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (flag[i]) {
+                for (int j = 0; j < arr[i]; j++) {
+                    list.add(arr[i]);
+                }
+            } else {
+                for (int j = 0; j < arr[i]; j++) {
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
 }

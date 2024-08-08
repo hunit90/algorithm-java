07544dae744707df9604d4ec8c0b0e3774c949e8
hunit <hunit612@naver.com>
2024-08-09 00:29:43 +0900

    programmers

--- src/programmers/level0/P181872.java
+++ src/programmers/level0/P181872.java
@@ -22,4 +22,22 @@ package programmers.level0;
  */
 
 public class P181872 {
+    public static void main(String[] args) {
+        solution("AbCdEFG", "dE");
+    }
+
+    public static String solution(String myString, String pat) {
+        String answer = "";
+        int patLength = pat.length();
+        int myStringLength = myString.length();
+
+        for (int i = myStringLength - 1; i >= 0; i--) {
+            String substr = myString.substring(0, i + 1);
+            if (substr.endsWith(pat)) {
+                answer = substr;
+                break;
+            }
+        }
+        return answer;
+    }
 }
